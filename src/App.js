5a0095aa81432d00f124afbef31c863521cc6cb2
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-27 12:20:58 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -2,5 +2,32 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return (
+    <>
+      <HelloWorldArticle />
+      <ByeWorldArticle />;
+    </>
+  );
+}
+
+function HelloWorldArticle() {
+  return (
+    <>
+      <article>
+        <h1>Hello World</h1>
+        <p>u suck</p>
+      </article>
+    </>
+  );
+}
+
+function ByeWorldArticle() {
+  return (
+    <>
+      <article>
+        <h2>Bye World</h2>
+        <p>u still suck</p>
+      </article>
+    </>
+  );
 }
